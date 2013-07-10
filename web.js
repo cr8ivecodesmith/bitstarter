eaa07f9b56043463e544b6cb736de5925a8feb0b
Matt Lebrun <cr8ivecodesmith@gmail.com>
2013-07-10 16:59:21 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var hello_string = fs.readFile('index.html');
+var hello_buffer = new Buffer(hello_string, 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello_buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;

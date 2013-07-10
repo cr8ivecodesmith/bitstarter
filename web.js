8391cc7c5c9e8e7b4234c118ce40c4d937989d84
Matt Lebrun <cr8ivecodesmith@gmail.com>
2013-07-10 17:20:34 +0000

    Attempting to fix web.js error.

--- web.js
+++ web.js
@@ -2,12 +2,10 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var hello_readfile = fs.readFile('index.html');
-var hello_buffer = new Buffer(hello_readfile, 'utf-8');
-var hello_string = hello_buffer.toString(); 
 
 app.get('/', function(request, response) {
-  response.send(hello_string);
+  var index_buffer = fs.readFile('index.html');
+  response.send(index_buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
